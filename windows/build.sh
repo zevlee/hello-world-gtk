438229c16d37bc2e1a349c84c822ec452c5b43eb
Zev Lee <60147316+zevlee@users.noreply.github.com>
2022-07-29 06:01:48 -0500

    Update build scripts
    PyInstaller bootloader needs to be built in order to use the development version

--- windows/build.sh
+++ windows/build.sh
@@ -9,7 +9,7 @@ echo "Setting up virtual environment..."
 python3 -m venv --system-site-packages venv
 . venv/bin/activate
 python3 -m pip install --upgrade pip
-python3 -m pip install -r ../requirements.txt
+PYINSTALLER_COMPILE_BOOTLOADER=1 PYI_STATIC_ZLIB=1 python3 -m pip install -r ../requirements.txt
 
 echo "Running pyinstaller..."
 
