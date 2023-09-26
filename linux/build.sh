f320dfacbdc7b9de03a4be9218c49011bbead2ae
Zev Lee <60147316+zevlee@users.noreply.github.com>
2023-09-25 20:51:30 -0500

    Remove compiler flags from build scripts
    Since we no longer build pyinstaller from source, there is no need to keep the compiler flags in the build scripts

--- linux/build.sh
+++ linux/build.sh
@@ -7,7 +7,7 @@ if [ ! -d ../venv ]; then
 	python3 -m venv --system-site-packages venv
 	. venv/bin/activate
 	python3 -m pip install --upgrade pip
-	PYINSTALLER_COMPILE_BOOTLOADER=1 PYI_STATIC_ZLIB=1 python3 -m pip install -r ../requirements.txt
+	python3 -m pip install -r ../requirements.txt
 else
 	. ../venv/bin/activate
 fi
