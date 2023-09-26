73ccee28f3d4942895e2a47e67ce273c0e1d4891
Zev Lee <60147316+zevlee@users.noreply.github.com>
2023-09-25 19:10:02 -0500

    Update windows bootstrap script
    C compiler is no longer needed since we don't build pyinstaller from scratch anymore

--- windows/bootstrap.sh
+++ windows/bootstrap.sh
@@ -2,6 +2,6 @@
 
 # This script is meant to be run through MinGW
 
-pacman -S --noconfirm mingw-w64-$(uname -m)-gtk4 mingw-w64-$(uname -m)-python-pip mingw-w64-$(uname -m)-python3-gobject mingw-w64-$(uname -m)-libadwaita mingw-w64-$(uname -m)-nsis mingw-w64-$(uname -m)-nsis-nsisunz mingw-w64-$(uname -m)-gcc zip unzip
+pacman -S --noconfirm mingw-w64-$(uname -m)-gtk4 mingw-w64-$(uname -m)-python-pip mingw-w64-$(uname -m)-python3-gobject mingw-w64-$(uname -m)-libadwaita mingw-w64-$(uname -m)-nsis mingw-w64-$(uname -m)-nsis-nsisunz zip unzip
 
 echo "Done"
