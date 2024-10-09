1c939f1f6bdbd41a7bade65602a7c12da5bbed26
Zev Lee <60147316+zevlee@users.noreply.github.com>
2024-10-09 13:37:11 -0500

    Revert "ci: Update release.yml"

--- linux/bootstrap.sh
+++ linux/bootstrap.sh
@@ -1,1 +1,1 @@
-apt install python3-gi python3-gi-cairo gir1.2-gtk-4.0
+apt install libgtk-4-1 libgtk-4-dev libadwaita-1-0 libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-4.0 gir1.2-adw-1
